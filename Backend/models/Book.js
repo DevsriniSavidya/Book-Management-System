bea934ca2ce57fd8c88d1ad6bbecfb4d62e9dcd8
Devsrini Savidya <savidyasamaranayake@gmail.com>
2025-01-30 15:32:17 +0530

    Add Book model

---
+++ Backend/models/Book.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const bookSchema = mongoose.Schema({
+    title: {
+        type:String,
+        required: true
+    },
+    author: {
+        type:String,
+        required: true
+    },
+    genre: {
+        type:String,
+        required: true
+    },
+    language: {
+        type:String,
+    },
+    publishDate: {
+        type:Date,
+        required: true
+    },
+    description: {
+        type:String,
+        required: true
+    }
+
+})
+
+export default mongoose.model("Book",bookSchema);
