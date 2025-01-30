0cbec9b850a0d1692ca64149a3e9b75e5e09c823
Devsrini Savidya <savidyasamaranayake@gmail.com>
2025-01-30 11:04:58 +0530

    Setup Express Server

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(process.env.CONNECTION_URL,).then(()=>app.listen(PORT,()=>
+    console.log(`server is running on port ${PORT}`)
+)).catch((error)=> console.log(error));
+
+
+
