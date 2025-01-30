ffcd31ff9e08cf8b945ecdf5dd98b7af4d4a7db6
Devsrini Savidya <savidyasamaranayake@gmail.com>
2025-01-31 00:46:30 +0530

    Frontend Initialize and Add tailwind styles

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import './index.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-x4l font-bold underline">
+        Hello world
+      </h1>
+
+<button className="bg-red-900 text-3xl">check</button>
     </div>
   );
 }
