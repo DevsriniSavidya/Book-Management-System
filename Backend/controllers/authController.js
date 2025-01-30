6a99585d06e891afd7d39c002ccadd366995f98b
Devsrini Savidya <savidyasamaranayake@gmail.com>
2025-01-30 13:01:18 +0530

    Add User Routes and authentications

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,38 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+
+export const register = async (req,res)=>{
+    try {
+        const {userName,email,password} = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({userName,email,password:hashedPassword});
+        await user.save();
+        res.status(201).json({message: "User registered Successfully!"})
+    } catch(error){
+        res.status(500).json({message:error.message})
+    }
+};
+
+export const login = async (req,res)=>{
+    try{
+        const {email,password} = req.body;
+        const user = await User.findOne({email});
+        if(!user)
+            return res.status(400).json({message:"User not found"});
+
+        const isValidPassword = await bcrypt.compare(password, user.password);
+        if (!isValidPassword)
+            return res.status(400).json({message: "Invalid credentials"});
+
+        const token = jwt.sign({ id:user._id }, process.env.JWT_SECRET_KEY ,{expiresIn:"10h"});
+        res.json({token,userId: user._id})
+    }catch(error){
+        res.status(500).json({message:error.message})
+    }
+}
+
+
+
+
--- Backend/server.js
+++ Backend/server.js
@@ -2,12 +2,16 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRouts from "./routes/authRouts.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+//Routes
+app.use("/api/auth", authRouts);
 
 const PORT = process.env.PORT || 5000
 
