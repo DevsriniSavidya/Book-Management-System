ced0f64c3d40ac0a7dda5639df19403688e07c22
Devsrini Savidya <savidyasamaranayake@gmail.com>
2025-01-30 19:14:14 +0530

    Update server

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRouts from "./routes/authRouts.js";
-
+import bookRouter from "./routes/bookRouter.js";
 dotenv.config();
 
 const app = express();
@@ -12,7 +12,7 @@ app.use(express.json());
 
 //Routes
 app.use("/api/auth", authRouts);
-
+app.use("/api/books",bookRouter)
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(process.env.CONNECTION_URL,).then(()=>app.listen(PORT,()=>
