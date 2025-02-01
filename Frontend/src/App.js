293a28a93c6fbb0a82e2ceae6d517d7ea8e5088e
Devsrini Savidya <savidyasamaranayake@gmail.com>
2025-02-01 22:56:24 +0530

    Update app js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,27 @@
-import './App.css';
-import './index.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import AddBook from "./pages/books/AddBook";
+import DisplayBook from "./pages/books/DisplayBook";
+import ViewBook from "./pages/books/ViewBook";
+import UpdateBook from "./pages/books/UpdateBook";
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="text-x4l font-bold underline">
-        Hello world
-      </h1>
-
-<button className="bg-red-900 text-3xl">check</button>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/addBook" element={<AddBook />} />
+                <Route path="/displayBook" element={<DisplayBook />} />
+                <Route path={"/viewBook"} element={<ViewBook />} />
+                <Route path="/update/:id" element={<UpdateBook />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
