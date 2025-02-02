22d3bbd3f6e131a2ec3ada062c941948d46e6b99
Devsrini Savidya <savidyasamaranayake@gmail.com>
2025-02-02 18:30:24 +0530

    Update app js (Add navbar and Logout)

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,23 +1,41 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./pages/Home";
+import { useState, useEffect } from "react";
+import Home from "./pages/Home";
+import AppNavbar from "./components/NavBar";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Home from "./pages/Home";
 import AddBook from "./pages/books/AddBook";
 import DisplayBook from "./pages/books/DisplayBook";
 import ViewBook from "./pages/books/ViewBook";
 import UpdateBook from "./pages/books/UpdateBook";
 
 function App() {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        // Check if the user is logged , by looking for a token in localStorage
+        const token = localStorage.getItem("token");
+        if (token) {
+            setUser({});
+        }
+    }, []);
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        setUser(null);
+        window.location.reload()
+    };
+
     return (
         <Router>
+            <AppNavbar user={user} handleLogout={handleLogout} />
             <Routes>
+                <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/" element={<Home />} />
                 <Route path="/addBook" element={<AddBook />} />
                 <Route path="/displayBook" element={<DisplayBook />} />
-                <Route path={"/viewBook"} element={<ViewBook />} />
+                <Route path="/viewBook" element={<ViewBook />} />
                 <Route path="/update/:id" element={<UpdateBook />} />
             </Routes>
         </Router>
